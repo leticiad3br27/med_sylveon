ad59ff7126b98ae26663b5e1b1d193a6e4d77346
leticiad3br27 <leticiaSilva271107@gmail.com>
2025-02-01 18:38:21 -0400

    certamente vai dar tudo errado

--- src/app/medico/page.js
+++ src/app/medico/page.js
@@ -1,8 +1,7 @@
-// app/medico/page.js
 "use client";
 
 import { useEffect, useState } from 'react';
-import { listarMedicos } from '@/app/Api/medicoApi'; // Corrigir o caminho da importação
+import { listarMedicos, buscarMedicoPorEspecialidade } from '@app/clinica-medica-api/servico/servicoConsultas';
 import Link from 'next/link';
 import styles from './MedicoPage.module.css';
 
@@ -10,27 +9,47 @@ export default function MedicoPage() {
   const [medicos, setMedicos] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [especialidade, setEspecialidade] = useState('');
+
+  const fetchMedicos = async (especialidade = '') => {
+    try {
+      const data = especialidade ? await buscarMedicoPorEspecialidade(especialidade) : await listarMedicos();
+      console.log('Dados recebidos:', data);
+      setMedicos(data);
+    } catch (err) {
+      console.error('Erro ao carregar médicos:', err);
+      setError(err.message || 'Erro ao carregar médicos');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchMedicos = async () => {
-      try {
-        const data = await listarMedicos();
-        console.log('Dados recebidos:', data); // Adicionado console.log para verificar os dados recebidos
-        setMedicos(data);
-      } catch (err) {
-        console.error('Erro ao carregar médicos:', err); // Adicionado console.log para verificar o erro
-        setError('Erro ao carregar médicos');
-      } finally {
-        setLoading(false);
-      }
-    };
-
     fetchMedicos();
   }, []);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (especialidade.trim() === '') {
+      setError('Por favor, insira uma especialidade válida.');
+      return;
+    }
+    setLoading(true);
+    fetchMedicos(especialidade);
+  };
 
   return (
     <div className={styles.div}>
       <h1 className={styles.h1}>Lista de Médicos</h1>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={especialidade}
+          onChange={(e) => setEspecialidade(e.target.value)}
+          placeholder="Buscar por especialidade"
+        />
+        <button type="submit">Buscar</button>
+      </form>
       {loading && <p className={styles.loading}>Carregando...</p>}
       {error && <p className={styles.error}>{error}</p>}
       {!loading && !error && (
@@ -49,7 +68,7 @@ export default function MedicoPage() {
                   <Link href={`/medico/${medico.id}`} className={styles.link}>{medico.nome}</Link>
                 </td>
                 <td>{medico.especialidade}</td>
-                <td>{medico.contato}</td>
+                <td>{medico.telefone}</td>
               </tr>
             ))}
           </tbody>
