16b84a2c3a2b973884b187c636f0d0676bd89292
LeticiaBJ <almeida.leticia@estudante.ifro.edu.br>
2025-02-05 08:24:38 -0400

    editei

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,32 +11,50 @@ const Header = () => {
             <Link href="/medico" className={styles.link}>Médico</Link>
             <ul className={styles.submenu}>
               <li className={styles.subitem}>
-                <Link href="/medico/agenda" className={styles.sublink}>Agenda</Link>
+                <Link href="/medico/agenda" className={styles.sublink}>Listar</Link>
               </li>
               <li className={styles.subitem}>
-                <Link href="/medico/prontuarios" className={styles.sublink}>Prontuários</Link>
+                <Link href="/medico/criar" className={styles.sublink}>Criar</Link>
+              </li>
+              <li className={styles.subitem}>
+                <Link href="/medico/editar" className={styles.sublink}>editar</Link>
+              </li>
+              <li className={styles.subitem}>
+                <Link href="/medico/Excluir" className={styles.sublink}>Excluir</Link>
               </li>
             </ul>
           </li>
           <li className={styles.item}>
             <Link href="/paciente" className={styles.link}>Paciente</Link>
             <ul className={styles.submenu}>
-              <li className={styles.subitem}>
-                <Link href="/paciente/agendamentos" className={styles.sublink}>Agendamentos</Link>
+            <li className={styles.subitem}>
+                <Link href="/paciente/agenda" className={styles.sublink}>Listar</Link>
               </li>
               <li className={styles.subitem}>
-                <Link href="/paciente/resultados" className={styles.sublink}>Resultados de Exames</Link>
+                <Link href="/paciente/criar" className={styles.sublink}>Criar</Link>
+              </li>
+              <li className={styles.subitem}>
+                <Link href="/paciente/editar" className={styles.sublink}>editar</Link>
+              </li>
+              <li className={styles.subitem}>
+                <Link href="/paciente/Excluir" className={styles.sublink}>Excluir</Link>
               </li>
             </ul>
           </li>
           <li className={styles.item}>
             <Link href="/consultas" className={styles.link}>Consultas</Link>
             <ul className={styles.submenu}>
-              <li className={styles.subitem}>
-                <Link href="/consultas/agendar" className={styles.sublink}>Agendar</Link>
+            <li className={styles.subitem}>
+                <Link href="/Consulta/agenda" className={styles.sublink}>Listar</Link>
               </li>
               <li className={styles.subitem}>
-                <Link href="/consultas/historico" className={styles.sublink}>Histórico</Link>
+                <Link href="/Consultas/criar" className={styles.sublink}>Criar</Link>
+              </li>
+              <li className={styles.subitem}>
+                <Link href="/Consultas/editar" className={styles.sublink}>editar</Link>
+              </li>
+              <li className={styles.subitem}>
+                <Link href="/Consultas/Excluir" className={styles.sublink}>Excluir</Link>
               </li>
             </ul>
           </li>
