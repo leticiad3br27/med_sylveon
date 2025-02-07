5d393b96f8f41c363037f1724528b3faa75ac6f6
LeticiaBJ <almeida.leticia@estudante.ifro.edu.br>
2025-02-07 08:24:12 -0400

    editei muita coisa

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,60 +1,62 @@
-'use client'
+'use client';
 import Link from 'next/link';
 import styles from './Header.module.css';
 
 const Header = () => {
   return (
     <header className={styles.header}>
+      <img className={styles.Icon} src="/Icon.png" alt="Icon" />
       <nav className={styles.nav}>
+        
         <ul className={styles.list}>
           <li className={styles.item}>
             <Link href="/medicoLista" className={styles.link}>Médico</Link>
             <ul className={styles.submenu}>
               <li className={styles.subitem}>
-                <Link href="/medicoLista" className={styles.sublink}>Listar</Link>
+                <Link href="/medico/listar" className={styles.sublink}>Listar</Link>
               </li>
               <li className={styles.subitem}>
                 <Link href="/medico/criar" className={styles.sublink}>Criar</Link>
               </li>
               <li className={styles.subitem}>
-                <Link href="/medico/editar" className={styles.sublink}>editar</Link>
+                <Link href="/medico/editar" className={styles.sublink}>Editar</Link>
               </li>
               <li className={styles.subitem}>
-                <Link href="/medico/Excluir" className={styles.sublink}>Excluir</Link>
+                <Link href="/medico/excluir" className={styles.sublink}>Excluir</Link>
               </li>
             </ul>
           </li>
           <li className={styles.item}>
             <Link href="/paciente" className={styles.link}>Paciente</Link>
             <ul className={styles.submenu}>
-            <li className={styles.subitem}>
-                <Link href="/paciente" className={styles.sublink}>Listar</Link>
+              <li className={styles.subitem}>
+                <Link href="/paciente/listar" className={styles.sublink}>Listar</Link>
               </li>
               <li className={styles.subitem}>
                 <Link href="/paciente/criar" className={styles.sublink}>Criar</Link>
               </li>
               <li className={styles.subitem}>
-                <Link href="/paciente/editar" className={styles.sublink}>editar</Link>
+                <Link href="/paciente/editar" className={styles.sublink}>Editar</Link>
               </li>
               <li className={styles.subitem}>
-                <Link href="/paciente/Excluir" className={styles.sublink}>Excluir</Link>
+                <Link href="/paciente/excluir" className={styles.sublink}>Excluir</Link>
               </li>
             </ul>
           </li>
           <li className={styles.item}>
             <Link href="/consultas" className={styles.link}>Consultas</Link>
             <ul className={styles.submenu}>
-            <li className={styles.subitem}>
-                <Link href="/Consulta/agenda" className={styles.sublink}>Listar</Link>
+              <li className={styles.subitem}>
+                <Link href="/consultas/agenda" className={styles.sublink}>Listar</Link>
               </li>
               <li className={styles.subitem}>
-                <Link href="/Consultas/criar" className={styles.sublink}>Criar</Link>
+                <Link href="/consultas/criar" className={styles.sublink}>Criar</Link>
               </li>
               <li className={styles.subitem}>
-                <Link href="/Consultas/editar" className={styles.sublink}>editar</Link>
+                <Link href="/consultas/editar" className={styles.sublink}>Editar</Link>
               </li>
               <li className={styles.subitem}>
-                <Link href="/Consultas/Excluir" className={styles.sublink}>Excluir</Link>
+                <Link href="/consultas/excluir" className={styles.sublink}>Excluir</Link>
               </li>
             </ul>
           </li>
